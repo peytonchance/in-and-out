51b825d06c509b0d2f7d9d41919fff4a71f05f5c
peytonchance <peytonchance@gmail.com>
2017-11-06 18:57:04 -0500

    Modified C3 graphics to pull data from external JSON.

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,54 @@
 var data;
+var barData;
+var enrollmentData;
+var revenueData;
+
+var inStatePercent = [];
+var outStatePercent = [];
+var uncEnroll = [];
+var ucbEnroll = [];
+var uvaEnroll = [];
+var schoolName = [];
+var year = [];
+var uncRev = [];
+var uvaRev = [];
+var ucbRev = [];
 
 
-
-function loadData(){
+// Bar Chart functions
+function loadBarData(){
   $.ajax({
     type: "GET",
-    url: "data/tuition.json",
+    url: "data/in-out-ratio.json",
     dataType: "json",
-    success: parseData
+    success: parseBarData
   });
+
 }
 
-function parseData(data){
-var tuitionYear, inState, outState;
-var tuitionByYearDict = {};
+function parseBarData(barData){
 
-for (var i = 0; i < data.length; i++){
-    tuitionYear = data[i].Year;
-    inState = data[i].In;
-    outState = data[i].Out;
-    tuitionYear = tuitionYear.toString();
+    for (var i=0; i < barData.length; i++){
 
+      if (i == 0){
 
-    if(tuitionByYearDict[tuitionYear] == undefined){
-      tuitionByYearDict[tuitionYear] = [inState, outState];
-    } else{
-      tuitionByYearDict[tuitionYear].push(inState, outState);
+      }else{
+        schoolName.push(barData[i]["schoolName"]);
+      }
+
+      inStatePercent.push(barData[i]["inStatePercent"]);
+      outStatePercent.push(barData[i]["outStatePercent"]);
     }
+    buildBarChart();
 }
 
-//// Get user input and work the magic
-var userYear = document.getElementById("userYear").value;
-
-$('#in-state-btn').change(function(){
-  $('#your-tuition').html(tuitionByYearDict[userYear][0]);
-  $('#parents-tuition').html(tuitionByYearDict[userYear - 25][0]);
-  $('#gparents-tuition').html(tuitionByYearDict[userYear - 50][0]);
-});
-
-$('#out-state-btn').change(function(){
-  $('#your-tuition').html(tuitionByYearDict[userYear][1]);
-  $('#parents-tuition').html(tuitionByYearDict[userYear - 25][1]);
-  $('#gparents-tuition').html(tuitionByYearDict[userYear - 50][1]);
-});
-
-
-}
-
-$(document).ready(function() {
+function buildBarChart(){
   var inOutStackedBar = c3.generate({
     bindto: '#inOutStackedBar',
     data: {
         columns: [
-            ['% In-State', .82, .76, .7],
-            ['% Out-of-State', .18, .24, .3],
+            inStatePercent,
+            outStatePercent,
         ],
         type: 'bar',
         groups: [
@@ -66,7 +61,7 @@ $(document).ready(function() {
     axis: {
          x: {
             type: 'category',
-            categories: ['UNC-Chapel Hill', 'U-Va.', 'UC-Berkeley']
+            categories:  schoolName
         },
         y: {
             tick:{
@@ -77,7 +72,31 @@ $(document).ready(function() {
         }
   }
   });
+}
+// End Bar Chart functions
 
+// Being EnrollmentLine functions
+
+function loadEnrollmentLineData(){
+  $.ajax({
+    type: "GET",
+    url: "data/enrollment-data.json",
+    dataType: "json",
+    success: parseEnrollmentLineData
+  })
+}
+
+function parseEnrollmentLineData(enrollmentData){
+  for (var i=0; i < enrollmentData.length; i++){
+
+    uncEnroll.push(enrollmentData[i]["UNC"]);
+    ucbEnroll.push(enrollmentData[i]["UVA"]);
+    uvaEnroll.push(enrollmentData[i]["Berkeley"]);
+  }
+  buildEnrollmentLine();
+}
+
+function buildEnrollmentLine(){
   var enrollmentLine = c3.generate({
     bindto: '#enrollmentLine',
     data: {
@@ -85,9 +104,9 @@ $(document).ready(function() {
         xFormat: '%Y',
         columns: [
             ['x', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'],
-            ['UNC-Chapel Hill', .17, .18, .17, .17, .19, .19, .19, .19, .19, .19, .19, .18],
-            ['U-Va.', .3, .33, .33, .33, .34, .34, .33, .34, .33, .33, .33, .30],
-            ['UC-Berkeley', .09, .09, .09, .09, .1, .13, .17, .19, .23, .24, .24, .24],
+            uncEnroll,
+            uvaEnroll,
+            ucbEnroll
         ]
     },
     axis: {
@@ -107,36 +126,120 @@ $(document).ready(function() {
         }
     }
 });
+}
+
+// End EnrollmentLine functions
+
+function loadRevLineData(){
+  $.ajax({
+    type: "GET",
+    url: "data/revenue-data.json",
+    dataType: "json",
+    success: parseRevData
+  });
+  console.log("test");
+}
+
+function parseRevData(revenueData){
+  console.log("test 2");
+  for (var i=0; i < revenueData.length; i++){
+    year.push(revenueData[i]["Year"]);
+    uncRev.push(revenueData[i]["UNC"]);
+    ucbRev.push(revenueData[i]["UVA"]);
+    uvaRev.push(revenueData[i]["Berkeley"]);
+    console.log("test");
+  }
+  // console.log(uncRev);
+  buildRevLine();
+}
+function buildRevLine(){
+  var revenueLine = c3.generate({
+      bindto: '#revenueLine',
+      data: {
+          x: 'x',
+          xFormat: '%Y',
+          columns: [
+              year,
+              uncRev,
+              uvaRev,
+              ucbRev,
+          ]
+      },
+      axis: {
+          x: {
+              type: 'timeseries',
+              tick: {
+                  format: '%Y'
+              }
+          },
+          y:{
+                tick:{
+                  format: d3.format('%')
+              },
+              max: 0.7,
+              min: 0,
+              padding: 0.1
+          }
+      }
+  });
+
+}
 
 
-var revenueLine = c3.generate({
-    bindto: '#revenueLine',
-    data: {
-        x: 'x',
-        xFormat: '%Y',
-        columns: [
-            ['x', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'],
-            ['UNC-Chapel Hill', .46, .46, .45, .47, .49, .47, .48, .47, .46, .49, .47, .46],
-            ['U-Va.', .59, .62, .62, .61, .63, .61, .60, .61, .61, .61, .59, .55],
-            ['UC-Berkeley', .26, .25, .24, .25, .27, .29, .34, .38, .43, .44, .45, .46],
-        ]
-    },
-    axis: {
-        x: {
-            type: 'timeseries',
-            tick: {
-                format: '%Y'
-            }
-        },
-        y:{
-              tick:{
-                format: d3.format('%')
-            },
-            max: 0.7,
-            min: 0,
-            padding: 0.1
-        }
+// Tuition Finder functions
+
+function loadData(){
+  $.ajax({
+    type: "GET",
+    url: "data/tuition.json",
+    dataType: "json",
+    success: parseData
+  });
+}
+
+function parseData(data){
+  var tuitionYear, inState, outState;
+  var tuitionByYearDict = {};
+
+for (var i = 0; i < data.length; i++){
+    tuitionYear = data[i].Year;
+    inState = data[i].In;
+    outState = data[i].Out;
+    tuitionYear = tuitionYear.toString();
+
+
+    if(tuitionByYearDict[tuitionYear] == undefined){
+      tuitionByYearDict[tuitionYear] = [inState, outState];
+    } else{
+      tuitionByYearDict[tuitionYear].push(inState, outState);
     }
-});
+}
+console.log(tuitionByYearDict);
 
+//// Get user input and work the magic
+var userYear = document.getElementById("userYear").value;
+console.log(document.getElementById("in-state-btn").checked);
+
+var tempBtn = document.getElementById("in-state-btn").checked
+
+if (tempBtn == true){
+  $('#your-tuition').html(tuitionByYearDict[userYear][0]);
+  $('#parents-tuition').html(tuitionByYearDict[userYear - 25][0]);
+  $('#gparents-tuition').html(tuitionByYearDict[userYear - 50][0]);
+} else{
+    console.log("else");
+    $('#your-tuition').html(tuitionByYearDict[userYear][1]);
+    $('#parents-tuition').html(tuitionByYearDict[userYear - 25][1]);
+    $('#gparents-tuition').html(tuitionByYearDict[userYear - 50][1]);
+
+
+}
+
+}
+
+// End Tuition Finder functions
+$(document).ready(function() {
+  loadBarData();
+  loadEnrollmentLineData();
+  loadRevLineData();
 } );
